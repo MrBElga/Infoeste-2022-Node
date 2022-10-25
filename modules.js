adb0ad2ac19f57990ee7d6c899e0cd506fe43372
Daniel Elias Fonseca Rumin <43758059+MrBElga@users.noreply.github.com>
2022-10-25 16:48:11 -0300

    Update modules.js

--- modules.js
+++ modules.js
@@ -1,4 +1,11 @@
 //import modules
 
 import add from "./modulos/add.js";
-import { sub } from "./modulos/sub.js";
+import { sub2 } from "./modulos/sub.js";
+
+const a=10;
+const b=5;
+const c=2;
+
+console.log(add(a,b));
+console.log(sub2(a,b,c));
