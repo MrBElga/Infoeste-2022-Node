1d0f586f32101a06f66076d5b071317448066e4c
Daniel Elias Fonseca Rumin <43758059+MrBElga@users.noreply.github.com>
2022-10-25 15:14:16 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,44 +1,52 @@
 //var, let e const 
 
-const x = 10;
-let   y = 20;
+// const x = 10;
+// let   y = 20;
 
-function hello(){
-    let y = 5;
-    console.log(y);
+// function hello(){
+//     let y = 5;
+//     console.log(y);
 
-    if(y==5)
-    {
-        let y=10;
-        console.log(y);
-    }
-}
+//     if(y==5)
+//     {
+//         let y=10;
+//         console.log(y);
+//     }
+// }
 
-hello();
-console.log(x);
-console.log(y);
+// hello();
+// console.log(x);
+// console.log(y);
 
-//Number
+// //Number
 
-let a = 20;
-let b = 2.5;
-let c = -2.5;
+// let a = 20;
+// let b = 2.5;
+// let c = -2.5;
 
-console.log(typeof a);
-console.log(typeof b);
-console.log(typeof c);
-console.log(a+b);
-console.log(a/b+c);
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+// console.log(a+b);
+// console.log(a/b+c);
 
 //String
 
-let nome = "Nome";
-let sobrenome = "Sobrenome";
+// let nome = "Nome";
+// let sobrenome = "Sobrenome";
 
-console.log(typeof nome);
-console.log(typeof sobrenome);
-console.log(nome+" "+sobrenome);
-console.log(nome.toUpperCase(),sobrenome.toUpperCase());
+// console.log(typeof nome);
+// console.log(typeof sobrenome);
+// console.log(nome+" "+sobrenome);
+// console.log(nome.toUpperCase(),sobrenome.toUpperCase());
+// ctrl c k
+// ctrl c u
 
 //JSON
+let obj ={
+    nome: "Nome",
+    sobrenome: "Sobrenome",
+    idade: 25
+};
 
+console.log(JSON.stringify(obj));
