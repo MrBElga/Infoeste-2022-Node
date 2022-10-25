8e74be14d5a90a97eeb8c49e819d58d0c45a43cc
Daniel Elias Fonseca Rumin <43758059+MrBElga@users.noreply.github.com>
2022-10-25 17:03:28 -0300

    att
    att

--- modules.js
+++ modules.js
@@ -2,6 +2,7 @@
 
 import add from "./modulos/add.js";
 import { sub2 } from "./modulos/sub.js";
+import moment from "moment-timezone";
 
 const a=10;
 const b=5;
@@ -9,3 +10,6 @@ const c=2;
 
 console.log(add(a,b));
 console.log(sub2(a,b,c));
+let hoje = moment.defineLocale("America/Los_Angeles");
+
+console.log(hoje);
